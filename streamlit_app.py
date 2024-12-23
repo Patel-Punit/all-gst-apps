233084b5457793b88d882bd7bc274fdc48ffbdc8
Patel-Punit <144874668+Patel-Punit@users.noreply.github.com>
2024-12-23 21:31:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,12 @@ import streamlit as st # type: ignore
 from pdf_main import pdf_main
 from excel_main import excel_main
 from json_main import json_main
+from reconcile_main import reconcile_main
 
 def main():
     st.sidebar.title("Select an App")
     # Create a radio button in the sidebar for app selection
-    app_selection = st.sidebar.radio("Choose App", ("PDF App", "Excel App", "JSON App"))
+    app_selection = st.sidebar.radio("Choose App", ("PDF App", "Excel App", "JSON App", "Reconcile App"))
 
     # Render the selected app
     if app_selection == "PDF App":
@@ -15,6 +16,8 @@ def main():
         excel_main()  # Call the excel_main function from the second app
     elif app_selection == "JSON App":
         json_main()
+    elif app_selection == "Reconcile App":
+        reconcile_main
 
 if __name__ == "__main__":
     main()
